411791d88c33968334bc1a9dbf081e6af47c932a
Chansung Park <deep.diver.csp@gmail.com>
2023-03-09 23:30:34 +0000

    add wandb configs

--- training_pipeline/pipeline/configs.py
+++ training_pipeline/pipeline/configs.py
@@ -4,7 +4,7 @@ import tfx.extensions.google_cloud_ai_platform.constants as vertex_const
 import tfx.extensions.google_cloud_ai_platform.trainer.executor as vertex_training_const
 import tfx.extensions.google_cloud_ai_platform.tuner.executor as vertex_tuner_const
 
-PIPELINE_NAME = "vit-e2e-pipeline-advanced-part2"
+PIPELINE_NAME = "tfx-vit-pipeline"
 
 try:
     import google.auth  # pylint: disable=g-import-not-at-top  # pytype: disable=import-error
@@ -59,6 +59,11 @@ HYPER_PARAMETERS = {
     }
 }
 
+WANDB_CONFIGS = {
+    "API_KEY": "",
+    "PROJECT": PIPELINE_NAME,
+}
+
 EVAL_CONFIGS = tfma.EvalConfig(
     model_specs=[
         tfma.ModelSpec(
@@ -111,6 +116,7 @@ GCP_AI_PLATFORM_TRAINING_ARGS = {
         ],
     },
     "use_gpu": True,
+    "wandb": WANDB_CONFIGS
 }
 
 fullres_data = os.environ.get("FULL_RES_DATA", "false")
@@ -189,6 +195,7 @@ GCP_AI_PLATFORM_TUNER_ARGS = {
     ),
     "use_gpu": True,
     "hyperparameters": HYPER_PARAMETERS,
+    "wandb": WANDB_CONFIGS
 }
 
 GCP_AI_PLATFORM_SERVING_ARGS = {
