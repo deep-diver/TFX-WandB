ce2e5f093e9524c671b4e037c8e6102870efdcf4
Chansung Park <deep.diver.csp@gmail.com>
2023-03-11 12:52:05 +0000

    add wandb pusher args

--- training_pipeline/kubeflow_runner.py
+++ training_pipeline/kubeflow_runner.py
@@ -36,6 +36,7 @@ def run():
             ai_platform_serving_args=configs.GCP_AI_PLATFORM_SERVING_ARGS,
             example_gen_beam_args=configs.EXAMPLE_GEN_BEAM_ARGS,
             transform_beam_args=configs.TRANSFORM_BEAM_ARGS,
+            wandb_pusher_args=configs.WANDB_PUSHER_ARGS,
         )
     )
 
